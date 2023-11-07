b7c74a8ee54dd03693208574dd43435862b03e60
AShadedBlobfish <119607801+AShadedBlobfish@users.noreply.github.com>
2023-11-07 13:35:49 +0000

    First vanilla edition release

--- src/main/java/com/elytra/ElytraRecipe.java
+++ src/main/java/com/elytra/ElytraRecipe.java
@@ -19,22 +19,12 @@ public class ElytraRecipe implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("elytra_recipe");
 
-	public static final Item KEVLAR = new Item(new FabricItemSettings());
-
-	public static final Item ENCHANTED_ENDER_EYE = new Item(new FabricItemSettings().maxCount(16));
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		Registry.register(Registries.ITEM, new Identifier("elytra_recipe", "kevlar"), KEVLAR);
-		Registry.register(Registries.ITEM, new Identifier("elytra_recipe", "enchanted_ender_eye"), ENCHANTED_ENDER_EYE);
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
-			content.addAfter(Items.LEATHER, KEVLAR);
-			content.addAfter(Items.ENDER_EYE, ENCHANTED_ENDER_EYE);
-		});
 		LOGGER.info("Mod loaded successfully");
 	}
 }
